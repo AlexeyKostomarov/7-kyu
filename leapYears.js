aed55df73d45d063219f523d5f553f4962b67a9e
Aleksey Kostomarov <kostomarov00@yandex.ru>
2023-11-01 14:58:40 +0300

    In this kata you should simply determine, whether a given year is a leap year or not.

---
+++ leapYears.js
@@ -0,0 +1,5 @@
+//In this kata you should simply determine, whether a given year is a leap year or not.
+
+function isLeapYear(year) {
+	return (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
+}
